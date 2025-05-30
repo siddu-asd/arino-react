40403047f00409056ffee6736f02155d51d8f7ec
siddu-asd <siddueee44@gmail.com>
2025-05-30 17:59:53 +0530

    latest updates

--- src/components/LogoList/index.jsx
+++ src/components/LogoList/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export default function TestSlider() {
@@ -44,26 +44,26 @@ export default function TestSlider() {
   return (
     <div style={{ width: '100%', maxWidth: '1300px', margin: '0 auto' }}>
       <Slider {...settings}>
-       <div><img src="/images/1.png" alt="Partner 1" /></div>
-<div><img src="/images/2.png" alt="Partner 2" /></div>
-<div><img src="/images/3.png" alt="Partner 3" /></div>
-<div><img src="/images/4.png" alt="Partner 4" /></div>
-<div><img src="/images/5.png" alt="Partner 5" /></div>
-<div><img src="/images/6.png" alt="Partner 6" /></div>
-<div><img src="/images/7.png" alt="Partner 7" /></div>
-<div><img src="/images/8.png" alt="Partner 8" /></div>
-<div><img src="/images/9.png" alt="Partner 9" /></div>
-<div><img src="/images/10.png" alt="Partner 10" /></div>
-<div><img src="/images/11.png" alt="Partner 11" /></div>
-<div><img src="/images/12.png" alt="Partner 12" /></div>
-<div><img src="/images/13.png" alt="Partner 13" /></div>
-<div><img src="/images/22.png" alt="Partner 14" /></div>
-<div><img src="/images/15.png" alt="Partner 15" /></div>
-<div><img src="/images/16.png" alt="Partner 16" /></div>
-<div><img src="/images/17.png" alt="Partner 17" /></div>
-<div><img src="/images/18.png" alt="Partner 18" /></div>
-<div><img src="/images/19.png" alt="Partner 19" /></div>
-<div><img src="/images/20.png" alt="Partner 20" /></div>
+        <div><img src="/images/1.png" alt="Partner 1" /></div>
+        <div><img src="/images/2.png" alt="Partner 2" /></div>
+        <div><img src="/images/3.png" alt="Partner 3" /></div>
+        <div><img src="/images/4.png" alt="Partner 4" /></div>
+        <div><img src="/images/5.png" alt="Partner 5" /></div>
+        <div><img src="/images/6.png" alt="Partner 6" /></div>
+        <div><img src="/images/7.png" alt="Partner 7" /></div>
+        <div><img src="/images/8.png" alt="Partner 8" /></div>
+        <div><img src="/images/9.png" alt="Partner 9" /></div>
+        <div><img src="/images/10.png" alt="Partner 10" /></div>
+        <div><img src="/images/11.png" alt="Partner 11" /></div>
+        <div><img src="/images/12.png" alt="Partner 12" /></div>
+        <div><img src="/images/13.png" alt="Partner 13" /></div>
+        <div><img src="/images/22.png" alt="Partner 14" /></div>
+        <div><img src="/images/15.png" alt="Partner 15" /></div>
+        <div><img src="/images/16.png" alt="Partner 16" /></div>
+        <div><img src="/images/17.png" alt="Partner 17" /></div>
+        <div><img src="/images/18.png" alt="Partner 18" /></div>
+        <div><img src="/images/19.png" alt="Partner 19" /></div>
+        <div><img src="/images/20.png" alt="Partner 20" /></div>
       </Slider>
     </div>
   );
