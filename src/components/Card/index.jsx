1726b9e8d1c66232b200db14f2c8f1aa3c6c1b7e
shareena-osa <shareena.osa@gmail.com>
2025-06-14 18:01:06 +0530

    UPDATE-SHAREENA

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,12 +3,18 @@ import { Link } from 'react-router-dom'
 import Div from '../Div'
 import './card.scss'
 
-export default function Card({title, link, src, alt}) {
+export default function Card({ title, link, src, alt }) {
   return (
     <Link to={link} className="cs-card cs-style1">
       <>
         <img src={src} alt={alt} />
-        <Div className="cs-card_overlay" />
+        
+        {/* Overlay with centered text */}
+        <Div className="cs-card_overlay">
+          <Div className="cs-overlay_text">{alt}</Div>
+        </Div>
+
+        {/* Title */}
         <Div className="cs-card_info">
           <h2 className="cs-card_title">{title}</h2>
         </Div>
@@ -16,3 +22,4 @@ export default function Card({title, link, src, alt}) {
     </Link>
   )
 }
+  
